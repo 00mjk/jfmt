f8dfe7b2007f0c319283886d792382df83a76051
Olivér <oliver.mrakovics@gmail.com>
2021-12-08 10:29:33 +0100

    Added Colorize.colorf method

--- src/main/java/com/oliverr/jfmt/Main.java
+++ src/main/java/com/oliverr/jfmt/Main.java
@@ -1,17 +1,18 @@
 package com.oliverr.jfmt;
 
+import com.oliverr.jfmt.colorize.Colorize;
 import com.oliverr.jfmt.formatter.Formatter;
 
 public class Main {
 
     private static final String github = "github.com/0l1v3rr/jfmt";
-    private static final String version = "v1.0.0";
+    private static final String version = "v1.1";
 
     public static void main(String[] args) {
-        System.out.println("\t--==<[{ Java Formatter }]>==--");
-        Formatter.printfln("GitHub: %v", github);
-        Formatter.printfln("Version: %v", version);
-        System.out.println("Read README.md for more information.");
+        System.out.println(Colorize.colorf("{t:yellow}\t--==<[{ Java Formatter }]>==--"));
+        System.out.println(Colorize.colorf(Formatter.stringf("{t:bgreen}GitHub:  {reset}%v", github)));
+        System.out.println(Colorize.colorf(Formatter.stringf("{t:bcyan}Version: {reset}%v", version)));
+        System.out.println(Colorize.colorf("{bg:black}{t:white}Read README.md for more information."));
     }
 
 }
---
+++ src/main/java/com/oliverr/jfmt/colorize/Colorize.java
@@ -0,0 +1,56 @@
+package com.oliverr.jfmt.colorize;
+
+import com.oliverr.jfmt.util.NotNull;
+import com.oliverr.jfmt.util.Replace;
+
+public class Colorize {
+
+    private static final Colors colors = new Colors();
+
+    public static String colorf(@NotNull String text) {
+        if(text == null) return "";
+
+        text = Replace.all(text, "{reset}", colors.getReset());
+        
+        text = Replace.all(text, "{t:black}", colors.textBlack);
+        text = Replace.all(text, "{t:red}", colors.textRed);
+        text = Replace.all(text, "{t:green}", colors.textGreen);
+        text = Replace.all(text, "{t:yellow}", colors.textYellow);
+        text = Replace.all(text, "{t:blue}", colors.textBlue);
+        text = Replace.all(text, "{t:magenta}", colors.textMagenta);
+        text = Replace.all(text, "{t:cyan}", colors.textCyan);
+        text = Replace.all(text, "{t:white}", colors.textWhite);
+        text = Replace.all(text, "{t:bblack}", colors.textBrightBlack);
+        text = Replace.all(text, "{t:bred}", colors.textBrightRed);
+        text = Replace.all(text, "{t:bgreen}", colors.textBrightGreen);
+        text = Replace.all(text, "{t:byellow}", colors.textBrightYellow);
+        text = Replace.all(text, "{t:bblue}", colors.textBrightBlue);
+        text = Replace.all(text, "{t:bmagenta}", colors.textBrightMagenta);
+        text = Replace.all(text, "{t:bcyan}", colors.textBrightCyan);
+        text = Replace.all(text, "{t:bwhite}", colors.textBrightWhite);
+        
+        text = Replace.all(text, "{bg:black}", colors.backgroundBlack);
+        text = Replace.all(text, "{bg:red}", colors.backgroundRed);
+        text = Replace.all(text, "{bg:green}", colors.backgroundGreen);
+        text = Replace.all(text, "{bg:yellow}", colors.backgroundYellow);
+        text = Replace.all(text, "{bg:blue}", colors.backgroundBlue);
+        text = Replace.all(text, "{bg:magenta}", colors.backgroundMagenta);
+        text = Replace.all(text, "{bg:cyan}", colors.backgroundCyan);
+        text = Replace.all(text, "{bg:white}", colors.backgroundWhite);
+        text = Replace.all(text, "{bg:bblack}", colors.backgroundBrightBlack);
+        text = Replace.all(text, "{bg:bred}", colors.backgroundBrightRed);
+        text = Replace.all(text, "{bg:bgreen}", colors.backgroundBrightGreen);
+        text = Replace.all(text, "{bg:byellow}", colors.backgroundBrightYellow);
+        text = Replace.all(text, "{bg:bblue}", colors.backgroundBrightBlue);
+        text = Replace.all(text, "{bg:bmagenta}", colors.backgroundBrightMagenta);
+        text = Replace.all(text, "{bg:bcyan}", colors.backgroundBrightCyan);
+        text = Replace.all(text, "{bg:bwhite}", colors.backgroundBrightWhite);
+
+        text = Replace.all(text, "{d:bold}", colors.bold);
+        text = Replace.all(text, "{d:reversed}", colors.reversed);
+        text = Replace.all(text, "{d:underline}", colors.underline);
+
+        return text+ colors.getReset();
+    }
+
+}
