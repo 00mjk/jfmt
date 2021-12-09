95a572e87f4519c3fee1251ef2fb38e475a3171f
Olivér <oliver.mrakovics@gmail.com>
2021-12-09 10:57:41 +0100

    added 256 color support

---
+++ src/main/java/com/oliverr/jfmt/colorize/rgb/Background256.java
@@ -0,0 +1,34 @@
+package com.oliverr.jfmt.colorize.rgb;
+
+import com.oliverr.jfmt.util.NotNull;
+
+public class Background256 extends Color256 {
+
+    private final String reset = "\u001b[0m";
+    private int code;
+    private String result;
+
+    public Background256(@NotNull int code) {
+        if(code < 0 || code > 255) {
+            throw new IllegalArgumentException("The code must be between 0 and 255.");
+        } else {
+            this.code = code;
+            result = "\u001b[48;5;" + code + "m";
+        }
+    }
+
+    @Override
+    public void print(@NotNull String text) { System.out.print(string(text)); }
+
+    @Override
+    public void println(@NotNull String text) { System.out.println(string(text)); }
+
+    @Override
+    public String string(@NotNull String text) { return result+text+reset; }
+
+    public void setCode(int code) {
+        this.code = code;
+        result = "\u001b[48;5;" + code + "m";
+    }
+
+}
---
+++ src/main/java/com/oliverr/jfmt/colorize/rgb/Color256.java
@@ -0,0 +1,12 @@
+package com.oliverr.jfmt.colorize.rgb;
+
+import com.oliverr.jfmt.util.NotNull;
+
+public abstract class Color256 {
+
+    public abstract void print(@NotNull String text);
+    public abstract void println(@NotNull String text);
+    public abstract String string(@NotNull String text);
+    public abstract void setCode(int code);
+
+}
